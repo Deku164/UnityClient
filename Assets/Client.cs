8b97af5d1dc9801bade4de16f4180195d15f582f
Deku164 <hhw1541@naver.com>
2019-05-03 17:44:26 +0900

    First Commit

---
+++ Assets/Client.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+using System.Net.Sockets;
+
+public class Client : MonoBehaviour
+{
+    private bool socketReady;
+    private TcpClient socket;
+    private NetworkStream stream;
+    private StreamWriter writer;
+    private StreamReader reader;
+
+    public void ConnectedToServer()
+    {
+        //if already connected, ignore this func
+        if (socketReady)
+            return;
+
+        //defalut host / port values
+        string host = "192.168.10.53";
+        int port = 80;
+
+        //overwrite default host / port values, if there is something in those boxes
+        string h;
+        int p;
+        h = GameObject.Find("HostInput").GetComponent<InputField>().text;
+        if (h != "")
+            host = h;
+        int.TryParse(GameObject.Find("PortInput").GetComponent<InputField>().text, out p);
+        if (p != 0)
+            port = p;
+
+        //create the socket
+        try
+        {
+            socket = new TcpClient(host, port);
+            stream = socket.GetStream();
+            writer = new StreamWriter(stream);
+            reader = new StreamReader(stream);
+            socketReady = true;
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error : " + e.Message);
+        }
+    }
+    private void Update()
+    {
+        if (socketReady)
+        {
+            if (stream.DataAvailable)
+            {
+                string data = reader.ReadLine();
+                if (data != null)
+                    OnIncomingData(data);
+            }
+        }
+    }
+    private void OnIncomingData(string data)
+    {        
+        Debug.Log(data);
+    }
+
+    private void Send(string data)
+    {
+        if (!socketReady)
+            return;
+
+        writer.WriteLine(data);
+        writer.Flush();
+    }
+
+    public void OnSendButton()
+    {
+        string message = GameObject.Find("BlockInput").GetComponent<InputField>().text;
+        Send(message);
+    }
+}
